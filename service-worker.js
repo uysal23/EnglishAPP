8e64f4a5e9dd75cdaae5646ea5d62f425e65f151
uysal23 <uysal_23@hotmail.com>
2026-09-19 16:16:28 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,31 @@
+const CACHE_NAME = 'willowbrook-v1';
+const URLS = ['./', './index.html', './manifest.json'];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((res) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, res.clone());
+          return res;
+        });
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
